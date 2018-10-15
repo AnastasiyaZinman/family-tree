60ddfb21d50b5ab453e2e0b8ab9df3aeb57b122e
AGCH <anastasiyache@gmail.com>
2018-10-15 14:48:54 +0300

    added options with user names in app

--- src/App.js
+++ src/App.js
@@ -9,39 +9,71 @@ import axios from 'axios';
 // @inject("store")
 // @observer 
 class App extends Component {
-    addUser(){
-       axios.post('http://localhost:5000/add_user', {name:"John Gold"}, {
-        headers: {
-            'Content-Type': 'application/json',
-        }
+  constructor() {
+    super();
+    this.state = {
+      users: ["one"],
+      selectedOption:""
     }
-    )
-    .then(response => {
-      console.log("response from DB",response);
-    })
-    .catch(function (error) {
-      alert("Sorry, something wrong. New client haven't added.");
-      console.log(error);
-    });
   }
-  getUsers() {
-    axios.get('http://localhost:5001/getUsers')
-    .then(result => {
-      console.log(result.data);
-      // this.setState({ clients: result.data })
+  generateOptions() {
+    return this.state.users.map((item, i) => {
+      // console.log("item",item);
+      let optionValue = item.name//(item.text!=="First Contact")? item.text.toLowerCase(): "firstContact";//item.text.split(' ').join('');
+      //item.text.split(' ').join('');            
+      return (<option
+        value={item.id}
+        key={i}>{optionValue}
+      </option>)   //the power of JSX!
+
     })
+  }
+
+  //   addUser(){
+  //      axios.post('http://localhost:5000/add_user', {name:"John Gold"}, {
+  //       headers: {
+  //           'Content-Type': 'application/json',
+  //       }
+  //   }
+  //   )
+  //   .then(response => {
+  //     console.log("response from DB",response);
+  //   })
+  //   .catch(function (error) {
+  //     alert("Sorry, something wrong. New client haven't added.");
+  //     console.log(error);
+  //   });
+  // }
+
+  updateSelectedOption = (e) => {
+    console.log("selected value",e.target.value);
+    this.setState({selectedOption: e.target.value})
+  }
+
+  componentDidMount() {
+    this.getUsers();
+  }
+  getUsers = () => {
+    axios.get('http://localhost:5001/getUsers')
+      .then(result => {
+        console.log(result.data);
+        this.setState({ users: result.data })
+      })
   }
 
   render() {
     return (
       <div className="App">
         <div>Some text</div>
-        
+
         {/* <SearchForm /> */}
         {/* <UserBoxs /> */}
+        <select onChange={this.updateSelectedOption}>
+          {this.generateOptions()}
+        </select>
+
         <button onClick={this.getUsers}>Add</button>
-         
-        
+
       </div>
     );
   }
