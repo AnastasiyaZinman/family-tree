6b5675cf24b9e4b4eaf984077c01b02f857de819
AGCH <anastasiyache@gmail.com>
2018-10-16 15:17:38 +0300

    add new User, but doesn't create parent row

---
+++ insertUpdate.sql
@@ -0,0 +1,5 @@
+-- UPDATE people
+-- SET imgUrl="https://image.flaticon.com/icons/png/512/163/163810.png"
+-- where id=8;
+INSERT INTO parents (personId,parentId)
+VALUES (9,3)
--- serverApis/usersApi.js
+++ serverApis/usersApi.js
@@ -3,54 +3,57 @@ const express = require('express')
 const sequelize = require('../dataAccess/da');
 //---------------------------------------
 const router = express.Router()
-const {Person,Parent} = require('../dataAccess/userModel');
+const { Person, Parent } = require('../dataAccess/userModel');
 var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
 
 //----------------Post-------------------
 //Add new user
-router.post('/add_user',jsonParser, function (req, res) {
-    // if (!req.body) return res.sendStatus(400);
-    console.log("here",req.body);
+router.post('/addChild', jsonParser,async (req, res) => {
+    if (!req.body) return res.sendStatus(400);
+    console.log("here", req.body);
     Person.create({
-        name: req.body.name   
-})
-    // Person.create(req.body)
-//  .then(user => res.json(user))
-    
+        name: req.body.name,
+        imgUrl:req.body.imgUrl
+    })
+        .then((users) => {
+            Parent.create({
+                personId: users.id,
+                parentId: req.body.id
+            })
+           
+        })
+        .then((result)=>{
+            res.json(result)
+        })
+        .error((err) => {
+            res.status(500).send(err);
+        })
+
 })
 //----------------Get-------------------
 
-router.get('/getUsers', async(req,res) => {
+router.get('/getUsers', async (req, res) => {
     Person.findAll()
-    .then(users => res.json(users))
+        .then(users => res.json(users))
 })
-router.get('/getChildren/:id', async(req,res) => {
-    userId=req.params.id;
-    console.log("userId",userId);
+
+router.get('/getChildren/:id', async (req, res) => {
+    userId = req.params.id;
+    console.log("userId", userId);
     Person.find({
-        include: [{model:Person, as:"Children"}],
-        where: {id: userId }
-      })
-    //   .then(results => 
-    //         results.movies.forEach(movie => {
-    //             console.log(movie.dataValues.m_name) 
-    //         })
-            // );
-
-
-    .then((users) =>{ 
-        res.json(users)})
-    .error((err)=>{
+        include: [{ model: Person, as: "Children" }],
+        where: { id: userId }
+    })
+        .then((users) => {
+            res.json(users)
+        })
+        .error((err) => {
             res.status(500).send(err);
         })
 })
 
-router.get('/addParent', async(req,res) => {
-    let person = await Person.find({  where:{name:""} })
-    let parent = await Parent.create({ name: "Julius" })
-    store.addCustomer(customer);
-})
+
 
 // const addRelationship = async function () {
 //     let person = await Person.create({ name: "Target" })
@@ -59,5 +62,5 @@ router.get('/addParent', async(req,res) => {
 //     //alternatively, could also do customer.addStore(store)
 // }
 // addRelationship()
-    
+
 module.exports = router;
